d4cf71d4d8671cbbe0a8f18142b685937a9e53aa
nandaribeir0 <fernanda.ribeiro126@etec.sp.gov.br>
2024-03-11 22:02:30 -0300

    Ola Fernanda- Finalização(teste)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
+    //Visualizar(view)- adicionar texto(text)
     <View style={styles.container}>
-      <Text>Open up App.to start working on your app!</Text>
+      <Text style={styles.field}>Olá Fernanda</Text>
+      <TextInput style={styles.txtField} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#6666ff',
+    //alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal:10,
   },
+  field:{
+    color: 'white',
+    fontSize: 30,
+  },
+  txtField: {
+    backgroundColor: 'rgb(204, 179, 255)',
+    width: 'auto',
+    borderRadius:10,
+    padding: 10,
+    marginTop:10,
+  }
 });
